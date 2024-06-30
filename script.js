d8f61af8bf22410c2bbdc2318d4eb7351d7f8a24
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2024-06-30 14:35:41 -0400

    fix

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function showTab(tabId) {
     var activeContent = document.getElementById(tabId);
     activeContent.classList.add('active');
 
-    // Optional: Highlight the active tab (not in CSS)
+    // Highlight the active tab
     var tabs = document.querySelectorAll('.tab');
     tabs.forEach(function(tab) {
         tab.classList.remove('active-tab');
@@ -37,33 +37,20 @@ function showTab(tabId) {
     activeTab.classList.add('active-tab');
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Define an array of objects with audio sources, corresponding text, and background GIFs
-    const audioTracks = [
-        { src: 'resources/audiotrack/memories.mp3', text: 'Playing Subwoofer Lullaby - By C418', background: 'resources/backgrounds/subwoofer.gif' },
-        { src: 'resources/audiotrack/oxygen.mp3', text: 'Playing Oxygene - By C418', background: 'resources/backgrounds/oxygen.gif' },
-        { src: 'resources/audiotrack/minecraft.mp3', text: 'Playing Minecraft - By C418', background: 'resources/backgrounds/minecraft.gif' },
-        { src: 'resources/audiotrack/swedes.mp3', text: 'Playing Sweden - By C418', background: 'resources/backgrounds/sweden.gif' },
-        { src: 'resources/audiotrack/technoblade.mp3', text: 'Playing Pigstep - By Lana Raines', background: 'resources/backgrounds/pigstep.gif' },
-        { src: 'resources/audiotrack/otherside.mp3', text: 'Playing Otherside - By Lana Raines', background: 'resources/backgrounds/otherside.gif' }
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // List of background images
+    const backgrounds = [
+      "resources/backgrounds/minecraft.gif",
+      "resources/backgrounds/sweden.gif",
+      "resources/backgrounds/otherside.gif"
     ];
 
-    // Select a random audio track from the array
-    const randomIndex = Math.floor(Math.random() * audioTracks.length);
-    const selectedTrack = audioTracks[randomIndex];
+    // Randomly select a background
+    const randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 
-    // Update the audio source and text
-    const audioElement = document.querySelector('audio');
-    audioElement.src = selectedTrack.src;
-    audioElement.addEventListener('loadedmetadata', function() {
-        audioElement.play(); // Autoplay the selected track
-    });
+    // Set the background image
+    document.querySelector('.background').style.backgroundImage = `url(${randomBackground})`;
 
-    // Update the audio widget text
-    const audioWidget = document.querySelector('.audio-widget');
-    audioWidget.textContent = selectedTrack.text;
-
-    // Update the background GIF
-    const backgroundDiv = document.querySelector('.background');
-    backgroundDiv.style.backgroundImage = `url(${selectedTrack.background})`;
 });
+
